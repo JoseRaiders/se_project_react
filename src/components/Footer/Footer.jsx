2594400b14fa97c7a0c1d64fad35c907106e3fa9
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-12-10 14:59:24 -0600

    add footer component and styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+function Footer() {
+  const currentYear = new Date().toLocaleString("default", {
+    year: "numeric",
+  });
+
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer__copyright">Developed by Anuska Jose</p>
+        <p className="footer__year">{currentYear}</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
