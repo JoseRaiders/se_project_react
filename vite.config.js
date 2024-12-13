a3b923490fef421a5d0d95a021764fb09eab90e2
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-12-13 09:36:03 -0600

    update project name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/wtwr_project/",
+  base: "/se_wtwr_react/",
   plugins: [react()],
   server: {
     port: 3000,
