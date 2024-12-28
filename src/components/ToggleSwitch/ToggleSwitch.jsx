d5f33fa286fc959501159fd57c47f5beb4df4cc0
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-12-28 14:05:54 -0600

    feat: enchance toggle switch functionality

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,7 +1,13 @@
-import Reach from "react";
+import React, { useState } from "react";
 import "./ToggleSwitch.css";
 
 function ToggleSwitch() {
+  const [currentTempUnit, setCurrentTempUnit] = useState("F");
+
+  const handleChange = () => {
+    setCurrentTempUnit(currentTempUnit === "F" ? "C" : "F");
+  };
+  // console.log(currentTempUnit);
   return (
     <div className="toggle">
       <label className="toggle__switch" htmlFor="toggle">
@@ -10,7 +16,30 @@ function ToggleSwitch() {
           type="checkbox"
           name="toggle"
           id="toggle"
+          aria-label="Toggle temperature unit"
+          onChange={handleChange}
         />
+        <span
+          className={
+            currentTempUnit === "F"
+              ? "toggle__switch-slider toggle__switch-slider-F"
+              : "toggle__switch-slider toggle__switch-slider-C"
+          }
+        ></span>
+        <p
+          className={`toggle__temp-F ${
+            currentTempUnit === "F" ? "toggle__switch-active-F" : ""
+          }`}
+        >
+          F
+        </p>
+        <p
+          className={`toggle__temp-C ${
+            currentTempUnit === "C" ? "toggle__switch-active-C" : ""
+          }`}
+        >
+          C
+        </p>
       </label>
     </div>
   );
